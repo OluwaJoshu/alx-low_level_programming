cba30c94c8b1dbba433276a3331a46695c193709
OluwaJoshu <olowajosh001@gmail.com>
2023-02-18 08:16:45 -0800

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,10 +8,12 @@
  */
 
 int main(void)
-{ char low;
-for (low = 'a'; low <= 'z' low++)
+{
+char low;
+for (low = 'a'; low <= 'z'; low++)
 putchar(low);
-for (Low = 'A'; low <= 'Z'; low++)
+for (low = 'A'; low <= 'Z'; low++)
 putchar(low);
-putchar('\n';
+putchar('\n')
 return (0);
+}
