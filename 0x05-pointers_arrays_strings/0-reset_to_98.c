f66fb75f298a058c06ccb7661a6d29736ba49ec7
OluwaJoshu <olowajosh001@gmail.com>
2023-02-28 18:17:20 -0800

    100-atoi

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * rset_to_98 - a function that takes a pointer to an int as a prameter
+ * reset_to_98 - a function that takes a pointer to an int as a prameter
  * and updates the value it points to 98
  * @n: input
  * Return: n
